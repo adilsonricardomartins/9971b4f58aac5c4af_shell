73fb51d6062a56748eaa7d2c2934918130129b21
adilsonricardomartins <148726832+adilsonricardomartins@users.noreply.github.com>
2023-10-22 20:58:01 -0300

    Update setup-shell.sh

--- setup-shell.sh
+++ setup-shell.sh
@@ -22,7 +22,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
-sudo apt-get install wget curl jq python3-certbot-dns-cloudflare opendkim opendkim-tools nodejs -y
+sudo apt-get update && sudo apt-get install wget curl jq python3-certbot-dns-cloudflare opendkim opendkim-tools nodejs -y
 
 sudo mkdir -p /root/.secrets && sudo chmod 0700 /root/.secrets/ && sudo touch /root/.secrets/cloudflare.cfg && sudo chmod 0400 /root/.secrets/cloudflare.cfg
 
